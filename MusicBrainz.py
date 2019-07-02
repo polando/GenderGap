fb4748a25172cb5b538c84c96d2b014441d1434c
Elif Alkaç <daldalelif88@gmail.com>
2019-07-02 18:47:33 +0200

    MusicBrainz crawl born in and all external links.

--- MusicBrainz.py
+++ MusicBrainz.py
@@ -41,12 +41,13 @@ def save_MB_artist_urls_to_file(artist_names,file_name):
     url = "https://musicbrainz.org/search?query={0}&type=artist&method=indexed"
     artist_url_list = list()
 
+    i = 1
     for artist_name in artist_names:
         soup_url = url.format(artist_name.replace(" ","+"))
         soup = get_soup(soup_url)
 
-        print(soup_url)
-
+        print(i,soup_url)
+        i = i + 1
         table = soup.find('table', {'class': 'tbl'})
         if table == None:
             artist_url_list.append([artist_name,None])
@@ -72,78 +73,140 @@ def get_MB_born(soup):
         return born[0:born.find('(')]
     return ''
 
+def get_MB_born_in(soup):
+    retVal = ''
+    born_in = soup.find('dd', {'class': 'begin_area'})
+    if born_in != None:
+        born_in_list = born_in.findAll('bdi')
+        if born_in_list != None:
+            for b in born_in_list:
+                retVal = retVal + b.getText() + ','
+            retVal = retVal[:-1]
+    return retVal
+
 def get_MB_area(soup):
+    retVal = ''
     area = soup.find('dd', {'class': 'area'})
     if area != None:
-        return area.find('bdi').getText()
-    return ''
+        areas = area.findAll('bdi')
+        if areas != None:
+            for a in areas:
+                retVal = retVal + a.getText() + ','
+            retVal = retVal[:-1]
+    return retVal
 
-def get_external_links(soup):
-    external_links = soup.find('ul', {'class': 'external_links'})
-    if external_links == None:
-        return {}
-
-
-    links = {}
-    wikidata = external_links.find('li', {'class': 'wikidata-favicon'})
-    songkick = external_links.find('li', {'class': 'songkick-favicon'})
-    residentadvisor = external_links.find('li', {'class': 'residentadvisor-favicon'})
-
-    if wikidata != None:
-        links['wikidata'] = wikidata.find('a').get('href')
-
-    if songkick != None:
-        links['songkick'] = songkick.find('a').get('href')
-
-    if residentadvisor != None:
-        links['residentadvisor'] = residentadvisor.find('a').get('href')
-
-    return links
-
-def save_MB_artist_info_to_file(file_name):
-    #df = pd.read_csv('data/MB/c_MB_artist_page_urls.tsv', sep='\t')
+def add_columns():
     df = pd.read_csv(file_name, sep='\t')
 
-    df['gender'] = ''
-    df['born'] = ''
-    df['area'] = ''
-    df['external_links'] = ''
+    df['born_in'] = ''
 
-    i=1
-    url = 'https://musicbrainz.org{0}'
-    for index, row in df.iterrows():
-        artist_url = row['artist_page_url']
-        if pd.isnull(artist_url) == False:
-            soup_url = url.format(artist_url)
-            soup = get_soup(soup_url)
-
-            row['gender'] = get_MB_gender(soup)
-            row['born'] = get_MB_born(soup)
-            row['area'] = get_MB_area(soup)
-            row['external_links'] = get_external_links(soup)
-
-            print(i,row['artist_name'],row['gender'],row['born'],row['area'],row['external_links'])
-
-        i = i+1
-
-    #df.to_csv('data/MB/c_MB_artist_page_urls.tsv', sep='\t', index=None, header=True)
     df.to_csv(file_name, sep='\t', index=None, header=True)
+
+def save_MB_artist_info_to_file(file_name, start_index):
+    df = pd.read_csv(file_name, sep='\t')
+    end_index = len(df)
+
+    for i in range(start_index, end_index):
+        df.iloc[i] = crawl_MB_artist_info(df.iloc[i])
+
+        if i % 1000 == 0:
+            df.to_csv(file_name, sep='\t', index=None, header=True)
+            df = pd.read_csv(file_name, sep='\t')
+
+        print(i, df.iloc[i]['artist_name'])
+
+def crawl_MB_artist_info(df_row):
+
+    url = 'https://musicbrainz.org{0}'
+
+    artist_url = df_row['artist_page_url']
+    if pd.isnull(artist_url) == False:
+        soup_url = url.format(artist_url)
+        soup = get_soup(soup_url)
+
+        df_row['gender'] = get_MB_gender(soup)
+        df_row['born'] = get_MB_born(soup)
+        df_row['born_in'] = get_MB_born_in(soup)
+        df_row['area'] = get_MB_area(soup)
+
+        #Get External Links
+        url_relationships = 'https://musicbrainz.org{0}/relationships'
+        soup_url = url_relationships.format(artist_url)
+        soup = get_soup(soup_url)
+
+        df_row['external_links'] = get_relationships(soup)
+
+    return df_row
+
+def get_relationships(soup):
+    relationships = {}
+    tables = soup.findAll('table', {'class': 'details'})
+    if tables != None:
+        for table in tables:
+            rows = table.findAll('tr')
+            if rows != None:
+                for r in rows:
+                    rel_type = r.find('th').getText()[:-1]
+                    rel_urls = ''
+
+                    rel_url = r.findAll('bdi')
+                    if rel_url != None:
+                        for u in rel_url:
+                            rel_urls = rel_urls + u.getText() + ' '
+                    relationships[rel_type] = rel_urls
+
+    return relationships
+
+def crawl_MB_artist_page_to_test(artist_page_url):
+    url = 'https://musicbrainz.org/artist{0}'
+    url_relationships = 'https://musicbrainz.org/artist{0}/relationships'
+    if pd.isnull(artist_page_url) == False:
+        soup_url = url.format(artist_page_url)
+        print(soup_url)
+        soup = get_soup(soup_url)
+
+        artist_info = {}
+        artist_info['gender'] = get_MB_gender(soup)
+        artist_info['born'] = get_MB_born(soup)
+        artist_info['born_in'] = get_MB_born_in(soup)
+        artist_info['area'] = get_MB_area(soup)
+
+
+
+        soup_url = url_relationships.format(artist_page_url)
+        print(soup_url)
+        soup = get_soup(soup_url)
+
+        artist_info['relationships'] = get_relationships(soup)
+
+        print(artist_info)
 
 if __name__ == '__main__':
     #RA : https://www.residentadvisor.net/
     #MB : https://musicbrainz.org/
 
-    crawl_artist_urls_from_MB = True
+    crawl_artist_urls_from_MB = False
     crawl_artists_from_MB = True
+    crawl_test = False
+    add_column = False
 
+    artist_url_to_test = '/0b0c25f4-f31c-46a5-a4fb-ccbf53d663bd'
     file_name = 'data/MB/MB_artist_page_urls.tsv'
 
     if crawl_artist_urls_from_MB == True:
         artist_names = read_RA_artist_names_from_file()
         #comment to crawl all artists
-        artist_names = artist_names[0:3000]
+        #artist_names = artist_names[0:3000]
 
         save_MB_artist_urls_to_file(artist_names,file_name)
 
     if crawl_artists_from_MB == True:
-        save_MB_artist_info_to_file(file_name)
+        save_MB_artist_info_to_file(file_name, 0)
+
+    if crawl_test == True:
+        crawl_MB_artist_page_to_test(artist_url_to_test)
+
+    if add_column == True:
+        add_columns()
+
+
