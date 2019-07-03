659fcdc2cdfa52f2fbb1151926b4c628dbbf2248
polando <cpt.m.qasemi@gmail.com>
2019-07-03 10:45:51 +0200

    Generate final pratyflock data Create translator script

--- Translator.py
+++ Translator.py
@@ -1,13 +1,37 @@
 from translate import Translator
 import pandas as pd
-from tqdm import tqdm
+import json
 
-tqdm.pandas()
+def makeDic(df):
+    countries = set(df['born in'])
+    dictionary = dict()
+    for country in countries:
+        if country is not None:
+            dictionary[country] = translator.translate(str(country))
+    return dictionary
+
+def saveDic(dictionary,file_address,dictName):
+    json.dump(dictionary, open(file_address + dictName , 'w', encoding="UTF-8"), ensure_ascii=False)
+
+def loadDic(file_address,dictName):
+    with open(file_address+dictName, encoding='utf-8') as fh:
+        dictionary = json.load(fh)
+    return dictionary
+
+
 file_address = 'data/PF/'
 file_name_source = 'PF_artists_combined.csv'
 file_name_dest = 'PF_artists_combined_translated.csv'
+dictName = "dict.json"
+translator = Translator(to_lang="en",from_lang="nl")
 df = pd.read_csv(file_address+file_name_source,encoding='UTF-8')
-translator = Translator(from_lang="nl",to_lang="en")
-df['gender'] = df['gender'].replace('vrouw' ,'woman');
-df['born in'] = df['born in'].progress_apply(lambda x: translator.translate(str(x)) if x is not None else x)
-df.to_csv(file_name_dest, index=False, encoding='UTF-8',header=True)
+#dictionary = makeDic(df)
+#saveDic(dictionary,file_address)
+dictionary = loadDic(file_address,dictName)
+df['born in'] = df['born in'].fillna("not given")
+df['born in'] = df['born in'].apply(lambda x: dictionary[x] if x is not None else x)
+df['gender'] = df['gender'].replace('vrouw','woman')
+df.to_csv(file_address+file_name_dest, sep='\t', index=None, header=True)
+
+
+
